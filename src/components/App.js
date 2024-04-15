5da2c0540a8b46401710a326f5757475e0c8f7f5
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-04-15 21:24:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,79 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React,{useState} from 'react'
+import Todos from './Todos'
+import Memo from './Memo'
+import Count from './Count'
+import { useEffect } from 'react'
 
-const App = ()=>{
-
-    const [todo, setTodo] = useState([]);
-
-    const [count, setCount] = useState(0);
-
-    const [skill, setSkill] = useState("");
-
-    const [memoSkill, setMemoSkill] = useState([]);
-
-    function handleTodo(){
-        setTodo([...todo, "New Todo"])
-    }
-
-    function handleCount(){
-        setCount(count+1);
-    }
-
-    function handleSkill(e){
-        setSkill(e.target.value);
-    }
-
-    function addMemo(){
-        if(skill.length < 5){
-            setSkill("");
-            return;
-        }
-        setMemoSkill([...memoSkill, skill]);
-        setSkill("");
-    }
-
+let App = () =>{
     useEffect(()=>{
-        document.title = `Count: ${count}`;
-    }, [count])
 
+    },[count,memo,todos])
+    let [todos,Settodos]=useState(['New Todo'])
+    let [memo,Setmemo]=useState(['HTMLL',"CSSS3"])
+    let [count,Setcount]=useState(1000000000)
     return(
-        <div id="main">
-            <h2>My Todos</h2>
-            <button id="add-todo-btn" 
-            onClick={handleTodo}> Add Todo </button>
-            {
-                todo.map((item, index)=>{
-                    return(
-                        <p id="todo-0" key={index}>{item}</p>
-                        
-                    )
-                })
-            }
-            <hr/>
-            <p>Count: </p>
-            <button id="incr-cnt"
-            onClick={handleCount}>{count}</button>
-            <h2>Expensive Calculation
-            <p id="calc">1000000000</p>
-            </h2>
-            <hr/>
-            <hr/>
-
-            <h2> Memo </h2>
-            <input id="skill-input" type="text" placeholder="Enter Skill"
-            onChange={handleSkill}
-            value={skill}></input>
-            <button id="skill-btn" onClick={addMemo}> Add Skill </button>
-            <ul id="item-jumbotron">
-                {
-                    memoSkill.map((item, index)=>{
-                        return(
-                            <li key={index}>{item}</li>
-                        )
-                    })
-                }
-            </ul>
+        <div id='main'>
+        <h1>React.useMEMo</h1>
+        <Todos set={Settodos} arr={todos} />
+        <Count set={Setcount} number={count}/>
+        <h1>Expensive Calculation</h1>
+        {/* <div id='calc'>1000000000</div> */}
+        <Memo set={Setmemo} arr={memo} />
         </div>
     )
+   
+        
 }
 
-export default App;
+export default App
